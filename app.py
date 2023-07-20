ef1e27e776b343cda8d4be36d88d031643d624bf
M Sai Sowmith <95735222+smth-27@users.noreply.github.com>
2023-07-20 18:23:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,62 +1,48 @@
-# main.py
 from flask import Flask, render_template, request, redirect, session
-from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.secret_key = "Smth"
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:hebn0jgp@localhost:5432/covid_vaccination"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app,session_options={"expire_on_commit": False})
-# User model
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
 
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
+# Configure the MongoDB connection
+app.config["MONGO_URI"] = "mongodb://localhost:8000/covid_vaccination"
+mongo = PyMongo(app)
 
-# VaccinationCenter model
-class VaccinationCenter(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    location = db.Column(db.String(100), nullable=False)
-    working_hours = db.Column(db.String(100), nullable=False)
+# User collection
+users = mongo.db.users
 
-    def __init__(self, name, location, working_hours):
-        self.name = name
-        self.location = location
-        self.working_hours = working_hours
+# VaccinationCenter collection
+vaccination_centers = mongo.db.vaccination_centers
 
-# Login route for users
-@app.route("/login", methods=["GET", "POST"])
+# User Use Cases
+@app.route("/html/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        user = User.query.filter_by(username=username, password=password).first()
+        user = users.find_one({"username": username, "password": password})
         if user:
-            session["user_id"] = user.id
+            session["user_id"] = str(user["_id"])
             return redirect("/dashboard")
         else:
             return render_template("login.html", error="Invalid username or password")
     return render_template("login.html", error="")
 
-# Sign up route for users
-@app.route("/signup", methods=["GET", "POST"])
+@app.route("/html/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        new_user = User(username=username, password=password)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect("/login")
-    return render_template("signup.html")
+        user = users.find_one({"username": username})
+        if user:
+            return render_template("signup.html", error="Username already exists.")
+        else:
+            user_data = {"username": username, "password": password}
+            users.insert_one(user_data)
+            return redirect("/login")
+    return render_template("signup.html", error="")
 
-# Dashboard route for users
-@app.route("/dashboard")
+@app.route("/html/dashboard")
 def dashboard():
     user_id = session.get("user_id")
     if user_id:
@@ -64,14 +50,14 @@ def dashboard():
     else:
         return redirect("/login")
 
-# Logout route for users
 @app.route("/logout")
 def logout():
     session.pop("user_id", None)
     return redirect("/login")
 
-# Login route for admin
-@app.route("/admin/login", methods=["GET", "POST"])
+# Admin Use Cases
+
+@app.route("/html/admin_login", methods=["GET", "POST"])
 def admin_login():
     if request.method == "POST":
         username = request.form["username"]
@@ -84,8 +70,7 @@ def admin_login():
             return render_template("admin_login.html", error="Invalid username or password")
     return render_template("admin_login.html", error="")
 
-# Admin dashboard route
-@app.route("/admin/dashboard")
+@app.route("/html/admin_dashboard")
 def admin_dashboard():
     admin_id = session.get("admin_id")
     if admin_id:
@@ -93,33 +78,26 @@ def admin_dashboard():
     else:
         return redirect("/admin/login")
 
-# Add Vaccination Centre route for admin
-@app.route("/admin/add_vaccination_center", methods=["GET", "POST"])
+@app.route("/html/add_vaccination_center", methods=["GET", "POST"])
 def add_vaccination_center():
     if request.method == "POST":
         name = request.form["name"]
         location = request.form["location"]
         working_hours = request.form["working_hours"]
-        new_center = VaccinationCenter(name=name, location=location, working_hours=working_hours)
-        db.session.add(new_center)
-        db.session.commit()
+        center_data = {"name": name, "location": location, "working_hours": working_hours}
+        vaccination_centers.insert_one(center_data)
         return redirect("/admin/dashboard")
     return render_template("add_vaccination_center.html")
 
-# Remove Vaccination Centre route for admin
-@app.route("/admin/remove_vaccination_center/<int:center_id>", methods=["POST"])
+@app.route("/admin/remove_vaccination_center/<string:center_id>", methods=["POST"])
 def remove_vaccination_center(center_id):
-    center = VaccinationCenter.query.get(center_id)
-    db.session.delete(center)
-    db.session.commit()
+    vaccination_centers.delete_one({"_id": center_id})
     return redirect("/admin/dashboard")
 
-# Vaccination Centre List route for users and admin
 @app.route("/vaccination_centers")
 def vaccination_centers():
-    centers = VaccinationCenter.query.all()
+    centers = vaccination_centers.find()
     return render_template("vaccination_centers.html", centers=centers)
 
 if __name__ == "__main__":
-    db.create_all()
-    app.run(debug=True)
+    app.run(debug=False)
